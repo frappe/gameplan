48b1d8250b4786e8beb8e62d29a9bc162c6e41f7
Faris Ansari <netchamp.faris@gmail.com>
2022-03-13 21:47:15 +0530

    chore: config to make feather-icons work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,6 +15,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  optimizeDeps: {
+    include: ['feather-icons'],
+  },
   build: {
     outDir: '../teams/public/frontend',
     emptyOutDir: true,
