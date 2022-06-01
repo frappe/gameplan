344a9ec48837faaeb95d95d209b34078d7004822
Faris Ansari <netchamp.faris@gmail.com>
2022-06-01 12:32:08 +0530

    fix: Set default route to /home

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    redirect: '/daily-planner',
+    redirect: '/home',
   },
   {
     path: '/daily-planner/:date?',
