4e00cbde082e403239be20bff599c474840c7e2c
Faris Ansari <netchamp.faris@gmail.com>
2022-07-13 20:22:47 +0530

    fix: use Dropdown component for UserDropdown

--- frontend/src/components/UserDropdown.vue
+++ frontend/src/components/UserDropdown.vue
@@ -1,65 +1,39 @@
 <template>
-  <Menu as="div" class="relative w-full" v-slot="{ open }">
-    <MenuButton
-      class="flex items-center w-full px-2 py-2 space-x-2 text-base font-medium text-left rounded-md"
-      :class="open ? 'bg-gray-300' : 'hover:bg-gray-200'"
-    >
-      <Avatar
-        :label="$user().full_name"
-        :imageURL="$user().user_image"
-        size="sm"
-      />
-      <span>
-        {{ $user().full_name }}
-      </span>
-      <FeatherIcon name="chevron-down" class="w-4 h-4" />
-    </MenuButton>
-    <transition
-      enter-active-class="transition"
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
-    >
-      <MenuItems
-        class="absolute left-0 w-full mt-0.5 rounded-md bg-white shadow-lg p-1"
+  <Dropdown
+    placement="center"
+    :options="[
+      {
+        label: 'My Profile',
+        route: { name: 'People', params: { person: 'profile' } },
+      },
+      { label: 'Settings', route: { name: 'Settings' } },
+    ]"
+  >
+    <template v-slot="{ open }">
+      <button
+        class="flex items-center w-full px-2 py-2 space-x-2 text-base font-medium text-left rounded-md"
+        :class="open ? 'bg-gray-300' : 'hover:bg-gray-200'"
       >
-        <MenuItem v-slot="{ active }">
-          <router-link
-            :to="{ name: 'People', params: { person: 'profile' } }"
-            :class="[
-              active ? 'bg-gray-100' : '',
-              'flex rounded-md text-gray-900 items-center w-full px-2 py-2 text-sm',
-            ]"
-          >
-            My Profile
-          </router-link>
-        </MenuItem>
-        <MenuItem v-slot="{ active }">
-          <router-link
-            to="/settings"
-            :class="[
-              active ? 'bg-gray-100' : '',
-              'flex rounded-md text-gray-900 items-center w-full px-2 py-2 text-sm',
-            ]"
-          >
-            Settings
-          </router-link>
-        </MenuItem>
-      </MenuItems>
-    </transition>
-  </Menu>
+        <Avatar
+          :label="$user().full_name"
+          :imageURL="$user().user_image"
+          size="sm"
+        />
+        <span>
+          {{ $user().full_name }}
+        </span>
+        <FeatherIcon name="chevron-down" class="w-4 h-4" />
+      </button>
+    </template>
+  </Dropdown>
 </template>
 <script>
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
-import FeatherIcon from 'frappe-ui/src/components/FeatherIcon.vue'
-import Avatar from 'frappe-ui/src/components/Avatar.vue'
-import Link from 'frappe-ui/src/components/Link.vue'
+import { FeatherIcon, Dropdown, Avatar, Link } from 'frappe-ui'
+
 export default {
   name: 'UserDropdown',
   components: {
-    Menu,
-    MenuButton,
-    MenuItems,
-    MenuItem,
+    Dropdown,
     FeatherIcon,
     Avatar,
     Link,
