05182ab5af79b34e4c0bbc83d8c88fc467ad297e
Faris Ansari <netchamp.faris@gmail.com>
2022-07-20 15:09:15 +0530

    fix: Pagination in DiscussionList

--- frontend/src/components/DiscussionList.vue
+++ frontend/src/components/DiscussionList.vue
@@ -1,58 +1,67 @@
 <template>
   <div class="h-full divide-y">
-    <template v-for="d in $resources.updates.data" :key="d.name">
-      <router-link
-        :to="{
-          name: this.routeName,
-          params: { teamId: d.team, projectId: d.project, postId: d.name },
-        }"
-        class="block p-3"
-        :class="isActive(d) ? 'bg-gray-100' : 'hover:bg-gray-50'"
-      >
-        <div class="flex items-center space-x-4">
-          <div>
-            <UserInfo :email="d.owner">
-              <template v-slot="{ user }">
-                <Avatar :label="user.full_name" :imageURL="user.user_image" />
-              </template>
-            </UserInfo>
-          </div>
+    <router-link
+      v-for="d in $resources.updates.data"
+      :key="d.name"
+      :to="{
+        name: this.routeName,
+        params: { teamId: d.team, projectId: d.project, postId: d.name },
+      }"
+      class="block p-3"
+      :class="isActive(d) ? 'bg-gray-100' : 'hover:bg-gray-50'"
+    >
+      <div class="flex items-center space-x-4">
+        <div>
           <UserInfo :email="d.owner">
             <template v-slot="{ user }">
-              <div class="flex items-start w-full">
-                <div>
-                  <div class="text-lg font-medium leading-snug">
-                    {{ d.title }}
-                  </div>
-                  <div class="mt-1 text-base text-gray-900">
-                    <span class="text-gray-600"> by </span>
-                    {{ user.full_name }}
-                    <template v-if="!filters || !filters.project">
-                      <span class="text-gray-600">in&nbsp;</span>
-                      <router-link
-                        class="hover:text-blue-600"
-                        :to="{
-                          name: 'ProjectDetailOverview',
-                          params: { teamId: d.team, projectId: d.project },
-                        }"
-                      >
-                        {{ d.team_title }} >
-                        {{ d.project_title }}
-                      </router-link>
-                    </template>
-                    <span class="text-gray-600" :title="$dayjs(d.creation)">
-                      &middot;
-                      {{ $dayjs(d.creation).fromNow() }}
-                    </span>
-                  </div>
-                </div>
-              </div>
+              <Avatar :label="user.full_name" :imageURL="user.user_image" />
             </template>
           </UserInfo>
         </div>
-      </router-link>
-    </template>
-    <div class="h-10"></div>
+        <UserInfo :email="d.owner">
+          <template v-slot="{ user }">
+            <div class="flex items-start w-full">
+              <div>
+                <div class="text-lg font-medium leading-snug">
+                  {{ d.title }}
+                </div>
+                <div class="mt-1 text-base text-gray-900">
+                  <span class="text-gray-600"> by </span>
+                  {{ user.full_name }}
+                  <template v-if="!filters || !filters.project">
+                    <span class="text-gray-600">in&nbsp;</span>
+                    <router-link
+                      class="hover:text-blue-600"
+                      :to="{
+                        name: 'ProjectDetailOverview',
+                        params: { teamId: d.team, projectId: d.project },
+                      }"
+                    >
+                      {{ d.team_title }} >
+                      {{ d.project_title }}
+                    </router-link>
+                  </template>
+                  <span class="text-gray-600" :title="$dayjs(d.creation)">
+                    &middot;
+                    {{ $dayjs(d.creation).fromNow() }}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </template>
+        </UserInfo>
+      </div>
+    </router-link>
+    <div class="p-3 pb-40 pl-14">
+      <Button
+        v-if="$resources.updates.hasNextPage"
+        class="ml-1"
+        @click="$resources.updates.next()"
+        :loading="$resources.updates.list.loading"
+      >
+        Load more posts
+      </Button>
+    </div>
   </div>
 </template>
 <script>
