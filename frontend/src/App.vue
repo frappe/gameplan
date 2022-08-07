361e13c4eaf8e30d4cacb6bf7f964bd766ab3adb
Faris Ansari <netchamp.faris@gmail.com>
2022-08-07 22:37:53 +0530

    fix: add dialog and toast container in root

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,18 +10,24 @@
       <router-view />
     </template>
   </AppLayout>
+  <Dialogs />
+  <Toasts />
 </template>
 
 <script>
 import AppLayout from './components/AppLayout.vue'
 import AppSidebar from './components/AppSidebar.vue'
 import AppNavbar from './components/AppNavbar.vue'
+import { Dialogs } from '@/utils/dialogs'
+import { Toasts } from '@/utils/toasts'
 
 export default {
   components: {
     AppLayout,
     AppSidebar,
     AppNavbar,
+    Dialogs,
+    Toasts,
   },
 }
 </script>
