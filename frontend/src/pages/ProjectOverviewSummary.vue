9ee9f8a9cfd5a8268191f55e21d697025d80beb9
Faris Ansari <netchamp.faris@gmail.com>
2022-10-19 01:19:58 +0530

    fix: clickable task summary cards
    closes #103

--- frontend/src/pages/ProjectOverviewSummary.vue
+++ frontend/src/pages/ProjectOverviewSummary.vue
@@ -1,9 +1,10 @@
 <template>
   <div v-if="cards.length" class="grid grid-cols-2 gap-5 sm:grid-cols-4">
-    <div
-      class="bg-gray-100 rounded-xl px-3.5 py-2.5"
+    <router-link
+      class="block rounded-xl bg-gray-100 px-3.5 py-2.5"
       v-for="card in cards"
       :key="card.subtitle"
+      :to="card.route"
     >
       <div
         :class="[
@@ -14,7 +15,7 @@
         {{ card.count }}
       </div>
       <div class="text-base text-gray-700">{{ card.subtitle }}</div>
-    </div>
+    </router-link>
   </div>
 </template>
 <script>
@@ -27,26 +28,50 @@ export default {
         return []
       }
 
+      let route = {
+        name: 'ProjectTasks',
+        params: {
+          teamId: this.project.doc.team,
+          projectId: this.project.doc.name,
+        },
+      }
+
       return [
         {
           count: this.project.doc.summary.completed_tasks,
           titleStyle: 'text-green-600',
           subtitle: 'Completed Tasks',
+          route: {
+            ...route,
+            query: { open: false },
+          },
         },
         {
           count: this.project.doc.summary.pending_tasks,
           titleStyle: 'text-yellow-600',
           subtitle: 'Pending Tasks',
+          route: {
+            ...route,
+            query: { open: true },
+          },
         },
         {
           count: this.project.doc.summary.overdue_tasks,
           titleStyle: 'text-red-700',
           subtitle: 'Overdue Tasks',
+          route: {
+            ...route,
+            query: { open: true },
+          },
         },
         {
           count: this.project.doc.summary.total_tasks,
           titleStyle: 'text-gray-700',
           subtitle: 'Total Tasks',
+          route: {
+            ...route,
+            query: { open: true },
+          },
         },
       ]
     },
