6c20c65b655a8f07f4ea1cbbd97c58bfcdea8aee
Faris Ansari <netchamp.faris@gmail.com>
2022-10-20 19:23:54 +0530

    fix: remove mobilenav from desktop layout
    - remove mobile related css

--- frontend/src/components/DesktopLayout.vue
+++ frontend/src/components/DesktopLayout.vue
@@ -3,33 +3,22 @@
     <div class="h-full flex-1">
       <div class="flex h-full">
         <div
-          class="relative hidden min-h-0 flex-shrink-0 overflow-hidden hover:overflow-auto sm:block"
+          class="relative block min-h-0 flex-shrink-0 overflow-hidden hover:overflow-auto"
         >
           <slot name="sidebar" />
           <AppSidebar />
         </div>
         <div class="w-full overflow-auto" id="scrollContainer">
           <div
-            class="flex items-center justify-between border-b py-4 px-4 sm:hidden"
-          >
-            <router-link
-              class="flex-shrink-0 font-bold uppercase tracking-widest text-gray-700 sm:w-64 sm:px-4"
-              :to="{ name: 'Home' }"
-            >
-              Gameplan
-            </router-link>
-            <MobileNav />
-          </div>
-          <div
             v-if="$readOnlyMode"
             class="right-0 top-0 mb-3 bg-gray-100 px-4 py-3 text-sm text-gray-600"
           >
-            <div class="mx-auto max-w-4xl px-4 sm:px-10">
+            <div class="mx-auto max-w-4xl px-10">
               This site is running in read-only mode. Full functionality will be
               restored soon.
             </div>
           </div>
-          <div class="mx-auto max-w-4xl px-4 sm:px-10">
+          <div class="mx-auto max-w-4xl px-10">
             <slot />
           </div>
         </div>
@@ -38,6 +27,5 @@
   </div>
 </template>
 <script setup>
-import MobileNav from './MobileNav.vue'
 import AppSidebar from './AppSidebar.vue'
 </script>
