d92aea4df46c2107231ed8258c89c9c5bfb58df0
Faris Ansari <netchamp.faris@gmail.com>
2022-11-06 18:56:15 +0530

    chore: add redisearch

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -33,6 +33,10 @@ echo "Setting Up Procfile..."
 sed -i 's/^watch:/# watch:/g' Procfile
 sed -i 's/^schedule:/# schedule:/g' Procfile
 
+echo "Setting up redisearch module..."
+echo "loadmodule ${GITHUB_WORKSPACE}/.github/helper/redisearch.so" >> ./config/redis_cache.conf
+cat ./config/redis_cache.conf
+
 echo "Starting Bench..."
 
 bench start &> bench_start.log &
