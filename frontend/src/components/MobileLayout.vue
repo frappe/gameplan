8f5914a465fbbcc148d15be81006c6542b81da7e
Faris Ansari <netchamp.faris@gmail.com>
2022-12-09 13:56:47 +0530

    fix: Hide tabs in mobile layout for guest

--- frontend/src/components/MobileLayout.vue
+++ frontend/src/components/MobileLayout.vue
@@ -3,7 +3,10 @@
     <div class="h-full overflow-auto">
       <slot />
     </div>
-    <div class="grid grid-cols-5 border-t border-gray-300 standalone:pb-4">
+    <div
+      class="grid grid-cols-5 border-t border-gray-300 standalone:pb-4"
+      :style="{ gridTemplateColumns: `repeat(${tabs.length}, minmax(0, 1fr))` }"
+    >
       <router-link
         v-for="tab in tabs"
         :key="tab.name"
@@ -52,12 +55,14 @@ export default {
           icon: 'users',
           route: { name: 'People' },
           isActive: /People|PersonProfile/g.test(this.$route.name),
+          condition: () => $user().isNotGuest,
         },
         {
           name: 'Search',
           icon: 'search',
           route: { name: 'Search' },
           isActive: this.$route.name === 'Search',
+          condition: () => $user().isNotGuest,
         },
         {
           name: 'Notifications',
@@ -65,7 +70,7 @@ export default {
           route: { name: 'Notifications' },
           isActive: this.$route.name === 'Notifications',
         },
-      ]
+      ].filter((tab) => (tab.condition ? tab.condition() : true))
     },
   },
 }
