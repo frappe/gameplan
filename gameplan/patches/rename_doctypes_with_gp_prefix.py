3193f7d745e27f8e6c28fed020b184bd4ff0bb81
Faris Ansari <netchamp.faris@gmail.com>
2023-01-12 18:00:17 +0530

    fix: patch to create sequences

--- gameplan/patches/rename_doctypes_with_gp_prefix.py
+++ gameplan/patches/rename_doctypes_with_gp_prefix.py
@@ -6,6 +6,11 @@ import frappe
 
 
 def execute():
+	rename_doctypes()
+	create_sequences()
+
+
+def rename_doctypes():
 	doctypes = {
 		'Team': 'GP Team',
 		'Team Project': 'GP Project',
@@ -24,3 +29,22 @@ def execute():
 		if not frappe.db.exists('DocType', new):
 			print('Renaming {0} to {1}'.format(old, new))
 			frappe.rename_doc('DocType', old, new, force=True, ignore_if_exists=True)
+
+def create_sequences():
+	doctypes = [
+		'GP Discussion',
+		'GP Comment',
+		'GP Project',
+		'GP Notification',
+		'GP Task',
+		'GP Discussion Visit',
+		'GP Member',
+		'GP Reaction',
+	]
+	for doctype in doctypes:
+		sequence_name = frappe.scrub(doctype + '_id_seq')
+		frappe.db.sql_ddl('drop sequence if exists {0}'.format(sequence_name))
+		last_name = frappe.db.get_all(doctype, fields=['max(name) as last'])[0].last
+		start_value = last_name + 1
+		print('Creating sequence for {0} starting at {1}'.format(doctype, start_value))
+		frappe.db.create_sequence(doctype, start_value=start_value, check_not_exists=True, cache=frappe.db.SEQUENCE_CACHE)
