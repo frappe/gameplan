c803db5d92df700ef95c4bde05d5b278857c42cb
Faris Ansari <netchamp.faris@gmail.com>
2023-06-15 14:10:13 +0530

    chore: use esm imports

--- frontend/src/utils/dayjs.js
+++ frontend/src/utils/dayjs.js
@@ -1,8 +1,8 @@
-import dayjs from 'dayjs'
+import dayjs from 'dayjs/esm'
 import relativeTime from 'dayjs/esm/plugin/relativeTime'
-import localizedFormat from 'dayjs/plugin/localizedFormat'
-import updateLocale from 'dayjs/plugin/updateLocale'
-import isToday from 'dayjs/plugin/isToday'
+import localizedFormat from 'dayjs/esm/plugin/localizedFormat'
+import updateLocale from 'dayjs/esm/plugin/updateLocale'
+import isToday from 'dayjs/esm/plugin/isToday'
 
 dayjs.extend(updateLocale)
 dayjs.extend(relativeTime)
