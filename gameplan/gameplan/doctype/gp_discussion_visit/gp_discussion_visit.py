fbd6ba70c43f5c1313dd436a8d78bf84454ac343
Faris Ansari <netchamp.faris@gmail.com>
2023-06-20 17:51:03 +0530

    fix: prevent duplicates in discussion list
    - add unique constraint on discussion visit

--- gameplan/gameplan/doctype/gp_discussion_visit/gp_discussion_visit.py
+++ gameplan/gameplan/doctype/gp_discussion_visit/gp_discussion_visit.py
@@ -5,13 +5,19 @@ import frappe
 import gameplan
 from frappe.model.document import Document
 
+
 class GPDiscussionVisit(Document):
 	def after_insert(self):
-		gameplan.refetch_resource('UnreadItems', user=self.user)
+		gameplan.refetch_resource("UnreadItems", user=self.user)
 
 	def on_change(self):
-		if self.has_value_changed('last_visit'):
-			gameplan.refetch_resource('UnreadItems', user=self.user)
+		if self.has_value_changed("last_visit"):
+			gameplan.refetch_resource("UnreadItems", user=self.user)
+
 
 def on_doctype_update():
-	frappe.db.add_index('GP Discussion Visit', ['discussion', 'user'])
+	frappe.db.add_index("GP Discussion Visit", ["discussion", "user"])
+
+
+def after_doctype_insert():
+	frappe.db.add_unique("GP Discussion Visit", ["discussion", "user"])
---
+++ gameplan/gameplan/doctype/gp_discussion_visit/patches/add_unique_constraint.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and Contributors
+# MIT License. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from gameplan.gameplan.doctype.gp_discussion_visit.gp_discussion_visit import after_doctype_insert
+
+
+def execute():
+	delete_duplicates()
+	after_doctype_insert()
+
+
+def delete_duplicates():
+	from frappe.query_builder.functions import Count
+
+	DiscussionVisit = frappe.qb.DocType("GP Discussion Visit")
+	duplicates = (
+		frappe.qb.from_(DiscussionVisit)
+		.select(DiscussionVisit.name)
+		.groupby(DiscussionVisit.discussion, DiscussionVisit.user)
+		.having(Count(DiscussionVisit.name) > 1)
+	).run(as_dict=1, pluck="name")
+
+	if duplicates:
+		for name in duplicates:
+			frappe.delete_doc_if_exists("GP Discussion Visit", name)
