c9ba8aaa657447fa6126e30a62252f8f96077d17
Faris Ansari <netchamp.faris@gmail.com>
2023-07-01 03:36:52 +0530

    chore: black formatting

--- gameplan/api.py
+++ gameplan/api.py
@@ -13,11 +13,9 @@ def get_user_info(user=None):
 	if frappe.session.user == "Guest":
 		frappe.throw("Authentication failed", exc=frappe.AuthenticationError)
 
-	filters = {
-		'roles.role': ['like', 'Gameplan %']
-	}
+	filters = {"roles.role": ["like", "Gameplan %"]}
 	if user:
-		filters['name'] = user
+		filters["name"] = user
 
 	users = frappe.qb.get_query(
 		"User",
@@ -27,13 +25,11 @@ def get_user_info(user=None):
 		distinct=True,
 	).run(as_dict=1)
 
-	roles = frappe.db.get_all('Has Role',
-		filters={'parenttype': 'User'},
-		fields=['role', 'parent']
-	)
-	user_profiles = frappe.db.get_all('GP User Profile',
-		fields=['user', 'name', 'image', 'image_background_color', 'is_image_background_removed'],
-		filters={'user': ['in', [u.name for u in users]]}
+	roles = frappe.db.get_all("Has Role", filters={"parenttype": "User"}, fields=["role", "parent"])
+	user_profiles = frappe.db.get_all(
+		"GP User Profile",
+		fields=["user", "name", "image", "image_background_color", "is_image_background_removed"],
+		filters={"user": ["in", [u.name for u in users]]},
 	)
 	user_profile_map = {u.user: u for u in user_profiles}
 	for user in users:
@@ -47,7 +43,7 @@ def get_user_info(user=None):
 			user.is_image_background_removed = user_profile.is_image_background_removed
 		user_roles = [r.role for r in roles if r.parent == user.name]
 		user.role = None
-		for role in ['Gameplan Guest', 'Gameplan Member', 'Gameplan Admin']:
+		for role in ["Gameplan Guest", "Gameplan Member", "Gameplan Admin"]:
 			if role in user_roles:
 				user.role = role
 	return users
@@ -57,14 +53,14 @@ def get_user_info(user=None):
 @validate_type
 def change_user_role(user: str, role: str):
 	if gameplan.is_guest():
-		frappe.throw('Only Admin can change user roles')
+		frappe.throw("Only Admin can change user roles")
 
-	if role not in ['Gameplan Guest', 'Gameplan Member', 'Gameplan Admin']:
+	if role not in ["Gameplan Guest", "Gameplan Member", "Gameplan Admin"]:
 		return get_user_info(user)[0]
 
-	user_doc = frappe.get_doc('User', user)
+	user_doc = frappe.get_doc("User", user)
 	for _role in user_doc.roles:
-		if _role.role in ['Gameplan Guest', 'Gameplan Member', 'Gameplan Admin']:
+		if _role.role in ["Gameplan Guest", "Gameplan Member", "Gameplan Admin"]:
 			user_doc.remove(_role)
 	user_doc.append_roles(role)
 	user_doc.save(ignore_permissions=True)
@@ -75,7 +71,7 @@ def change_user_role(user: str, role: str):
 @frappe.whitelist()
 @validate_type
 def remove_user(user: str):
-	user_doc = frappe.get_doc('User', user)
+	user_doc = frappe.get_doc("User", user)
 	user_doc.enabled = 0
 	user_doc.save(ignore_permissions=True)
 	return user
@@ -90,29 +86,28 @@ def invite_by_email(emails: str, role: str, projects: list = None):
 	email_list = split_emails(email_string)
 	if not email_list:
 		return
-	existing_members = frappe.db.get_all('User', filters={'email': ['in', email_list]}, pluck='email')
-	existing_invites = frappe.db.get_all('GP Invitation',
-		filters={
-			'email': ['in', email_list],
-			'role': ['in', ['Gameplan Admin', 'Gameplan Member']]
-		},
-		pluck='email')
-	to_invite = list(set(email_list) - set(existing_members) - set(existing_invites))
+	existing_members = frappe.db.get_all("User", filters={"email": ["in", email_list]}, pluck="email")
+	existing_invites = frappe.db.get_all(
+		"GP Invitation",
+		filters={"email": ["in", email_list], "role": ["in", ["Gameplan Admin", "Gameplan Member"]]},
+		pluck="email",
+	)
+
+	if role == "Gameplan Guest":
+		to_invite = list(set(email_list) - set(existing_invites))
+	else:
+		to_invite = list(set(email_list) - set(existing_members) - set(existing_invites))
+
 	if projects:
 		projects = frappe.as_json(projects, indent=None)
 
 	for email in to_invite:
-		frappe.get_doc(
-			doctype='GP Invitation',
-			email=email,
-			role=role,
-			projects=projects
-		).insert(ignore_permissions=True)
+		frappe.get_doc(doctype="GP Invitation", email=email, role=role, projects=projects).insert(ignore_permissions=True)
 
 
 @frappe.whitelist()
 def unread_notifications():
-	res = frappe.db.get_all('GP Notification', 'count(name) as count', {'to_user': frappe.session.user, 'read': 0})
+	res = frappe.db.get_all("GP Notification", "count(name) as count", {"to_user": frappe.session.user, "read": 0})
 	return res[0].count
 
 
@@ -122,13 +117,11 @@ def accept_invitation(key: str = None):
 	if not key:
 		frappe.throw("Invalid or expired key")
 
-	result = frappe.db.get_all(
-		"GP Invitation", filters={"key": key}, pluck='name'
-	)
+	result = frappe.db.get_all("GP Invitation", filters={"key": key}, pluck="name")
 	if not result:
 		frappe.throw("Invalid or expired key")
 
-	invitation = frappe.get_doc('GP Invitation', result[0])
+	invitation = frappe.get_doc("GP Invitation", result[0])
 	invitation.accept()
 	invitation.reload()
 
@@ -151,32 +144,36 @@ def get_unsplash_photos(keyword=None):
 @frappe.whitelist()
 def get_unread_items():
 	from frappe.query_builder.functions import Count
+
 	Discussion = frappe.qb.DocType("GP Discussion")
 	Visit = frappe.qb.DocType("GP Discussion Visit")
 	query = (
 		frappe.qb.from_(Discussion)
-			.select(Discussion.team, Count(Discussion.team).as_("count"))
-			.left_join(Visit)
-			.on((Visit.discussion == Discussion.name) & (Visit.user == frappe.session.user))
-			.where((Visit.last_visit.isnull()) | (Visit.last_visit < Discussion.last_post_at))
-			.groupby(Discussion.team)
+		.select(Discussion.team, Count(Discussion.team).as_("count"))
+		.left_join(Visit)
+		.on((Visit.discussion == Discussion.name) & (Visit.user == frappe.session.user))
+		.where((Visit.last_visit.isnull()) | (Visit.last_visit < Discussion.last_post_at))
+		.groupby(Discussion.team)
 	)
 
 	is_guest = gameplan.is_guest()
 	if is_guest:
-		GuestAccess = frappe.qb.DocType('GP Guest Access')
+		GuestAccess = frappe.qb.DocType("GP Guest Access")
 		project_list = GuestAccess.select(GuestAccess.project).where(GuestAccess.user == frappe.session.user)
 		query = query.where(Discussion.project.isin(project_list))
 
 	# pypika doesn't have any API for "FORCE INDEX FOR JOIN"
 	sql = query.get_sql()
-	sql = sql.replace('LEFT JOIN `tabGP Discussion Visit`', 'LEFT JOIN `tabGP Discussion Visit` FORCE INDEX FOR JOIN(discussion_user_index)')
+	sql = sql.replace(
+		"LEFT JOIN `tabGP Discussion Visit`", "LEFT JOIN `tabGP Discussion Visit` FORCE INDEX FOR JOIN(discussion_user_index)"
+	)
 	data = frappe.db.sql(sql, as_dict=1)
 
 	out = {}
 	for d in data:
 		out[d.team] = d.count
 	return out
+
 
 @frappe.whitelist()
 def get_unread_items_by_project(projects):
@@ -187,12 +184,12 @@ def get_unread_items_by_project(projects):
 	Visit = frappe.qb.DocType("GP Discussion Visit")
 	query = (
 		frappe.qb.from_(Discussion)
-			.select(Discussion.project, Count(Discussion.project).as_("count"))
-			.left_join(Visit)
-			.on((Visit.discussion == Discussion.name) & (Visit.user == frappe.session.user))
-			.where((Visit.last_visit.isnull()) | (Visit.last_visit < Discussion.last_post_at))
-			.where(Discussion.project.isin(project_names))
-			.groupby(Discussion.project)
+		.select(Discussion.project, Count(Discussion.project).as_("count"))
+		.left_join(Visit)
+		.on((Visit.discussion == Discussion.name) & (Visit.user == frappe.session.user))
+		.where((Visit.last_visit.isnull()) | (Visit.last_visit < Discussion.last_post_at))
+		.where(Discussion.project.isin(project_names))
+		.groupby(Discussion.project)
 	)
 
 	data = query.run(as_dict=1)
@@ -204,8 +201,8 @@ def get_unread_items_by_project(projects):
 
 @frappe.whitelist()
 def mark_all_notifications_as_read():
-	for d in frappe.db.get_all('GP Notification', filters={'to_user': frappe.session.user, 'read': 0}, pluck='name'):
-		doc = frappe.get_doc('GP Notification', d)
+	for d in frappe.db.get_all("GP Notification", filters={"to_user": frappe.session.user, "read": 0}, pluck="name"):
+		doc = frappe.get_doc("GP Notification", d)
 		doc.read = 1
 		doc.save(ignore_permissions=True)
 
@@ -214,28 +211,30 @@ def mark_all_notifications_as_read():
 def recent_projects():
 	from frappe.query_builder.functions import Max
 
-	ProjectVisit = frappe.qb.DocType('GP Project Visit')
-	Team = frappe.qb.DocType('GP Team')
-	Project = frappe.qb.DocType('GP Project')
-	Pin = frappe.qb.DocType('GP Pinned Project')
+	ProjectVisit = frappe.qb.DocType("GP Project Visit")
+	Team = frappe.qb.DocType("GP Team")
+	Project = frappe.qb.DocType("GP Project")
+	Pin = frappe.qb.DocType("GP Pinned Project")
 	pinned_projects_query = frappe.qb.from_(Pin).select(Pin.project).where(Pin.user == frappe.session.user)
 	projects = (
 		frappe.qb.from_(ProjectVisit)
-			.select(
-				ProjectVisit.project.as_('name'),
-				Project.team,
-				Project.title.as_('project_title'),
-				Team.title.as_('team_title'),
-				Project.icon,
-				Max(ProjectVisit.last_visit).as_('timestamp')
-			)
-			.left_join(Project).on(Project.name == ProjectVisit.project)
-			.left_join(Team).on(Team.name == Project.team)
-			.groupby(ProjectVisit.project)
-			.where(ProjectVisit.user == frappe.session.user)
-			.where(ProjectVisit.project.notin(pinned_projects_query))
-			.orderby(ProjectVisit.last_visit, order=frappe.qb.desc)
-			.limit(12)
+		.select(
+			ProjectVisit.project.as_("name"),
+			Project.team,
+			Project.title.as_("project_title"),
+			Team.title.as_("team_title"),
+			Project.icon,
+			Max(ProjectVisit.last_visit).as_("timestamp"),
+		)
+		.left_join(Project)
+		.on(Project.name == ProjectVisit.project)
+		.left_join(Team)
+		.on(Team.name == Project.team)
+		.groupby(ProjectVisit.project)
+		.where(ProjectVisit.user == frappe.session.user)
+		.where(ProjectVisit.project.notin(pinned_projects_query))
+		.orderby(ProjectVisit.last_visit, order=frappe.qb.desc)
+		.limit(12)
 	)
 
 	return projects.run(as_dict=1)
@@ -245,23 +244,25 @@ def recent_projects():
 def active_projects():
 	from frappe.query_builder.functions import Count
 
-	Comment = frappe.qb.DocType('GP Comment')
-	Discussion = frappe.qb.DocType('GP Discussion')
-	CommentCount = Count(Comment.name).as_('comments_count')
+	Comment = frappe.qb.DocType("GP Comment")
+	Discussion = frappe.qb.DocType("GP Discussion")
+	CommentCount = Count(Comment.name).as_("comments_count")
 	active_projects = (
 		frappe.qb.from_(Comment)
-			.select(CommentCount, Discussion.project)
-			.left_join(Discussion).on(Discussion.name == Comment.reference_name)
-			.where(Comment.reference_doctype == 'GP Discussion')
-			.where(Comment.creation > frappe.utils.add_days(frappe.utils.now(), -70))
-			.groupby(Discussion.project)
-			.orderby(CommentCount, order=frappe.qb.desc)
-			.limit(12)
+		.select(CommentCount, Discussion.project)
+		.left_join(Discussion)
+		.on(Discussion.name == Comment.reference_name)
+		.where(Comment.reference_doctype == "GP Discussion")
+		.where(Comment.creation > frappe.utils.add_days(frappe.utils.now(), -70))
+		.groupby(Discussion.project)
+		.orderby(CommentCount, order=frappe.qb.desc)
+		.limit(12)
 	).run(as_dict=1)
 
-	projects = frappe.qb.get_query('GP Project',
-		fields=['name', 'title as project_title', 'team', 'team.title as team_title', 'icon', 'modified as timestamp'],
-		filters={'name': ('in', [d.project for d in active_projects])}
+	projects = frappe.qb.get_query(
+		"GP Project",
+		fields=["name", "title as project_title", "team", "team.title as team_title", "icon", "modified as timestamp"],
+		filters={"name": ("in", [d.project for d in active_projects])},
 	).run(as_dict=1)
 
 	active_projects_comment_count = {d.project: d.comments_count for d in active_projects}
@@ -273,19 +274,15 @@ def active_projects():
 	return projects
 
 
-
-
-
-
-
 @frappe.whitelist()
 def onboarding(data):
 	data = frappe.parse_json(data)
-	team = frappe.get_doc(doctype='GP Team', title=data.team).insert()
-	frappe.get_doc(doctype='GP Project', team=team.name, title=data.project).insert()
-	emails = ', '.join(data.emails)
-	invite_by_email(emails, role='Gameplan Member')
+	team = frappe.get_doc(doctype="GP Team", title=data.team).insert()
+	frappe.get_doc(doctype="GP Project", team=team.name, title=data.project).insert()
+	emails = ", ".join(data.emails)
+	invite_by_email(emails, role="Gameplan Member")
 	return team.name
+
 
 @frappe.whitelist(allow_guest=True)
 def oauth_providers():
@@ -318,7 +315,7 @@ def oauth_providers():
 				{
 					"name": provider.name,
 					"provider_name": provider.provider_name,
-					"auth_url": get_oauth2_authorize_url(provider.name, '/g/home'),
+					"auth_url": get_oauth2_authorize_url(provider.name, "/g/home"),
 					"icon": icon,
 				}
 			)
--- gameplan/gameplan/doctype/gp_invitation/gp_invitation.py
+++ gameplan/gameplan/doctype/gp_invitation/gp_invitation.py
@@ -4,32 +4,31 @@
 import frappe
 from frappe.model.document import Document
 
+
 class GPInvitation(Document):
 	def before_insert(self):
 		frappe.utils.validate_email_address(self.email, True)
-		if self.role == 'Gameplan Guest' and not (self.teams or self.projects):
-			frappe.throw('Team or Project is required to invite as Guest')
+		if self.role == "Gameplan Guest" and not (self.teams or self.projects):
+			frappe.throw("Project is required to invite as Guest")
 
-		if self.role != 'Gameplan Guest':
+		if self.role != "Gameplan Guest":
 			self.teams = None
 			self.projects = None
 
 		self.key = frappe.generate_hash(length=12)
 		self.invited_by = frappe.session.user
-		self.status = 'Pending'
+		self.status = "Pending"
 
 	def after_insert(self):
 		self.invite_via_email()
 
 	def invite_via_email(self):
-		invite_link = frappe.utils.get_url(
-			f"/api/method/gameplan.api.accept_invitation?key={self.key}"
-		)
+		invite_link = frappe.utils.get_url(f"/api/method/gameplan.api.accept_invitation?key={self.key}")
 		if frappe.local.dev_server:
 			print(f"Invite link for {self.email}: {invite_link}")
 
-		title = f'Gameplan'
-		template = 'gameplan_invitation'
+		title = f"Gameplan"
+		template = "gameplan_invitation"
 
 		frappe.sendmail(
 			recipients=self.email,
@@ -38,33 +37,33 @@ class GPInvitation(Document):
 			args={"title": title, "invite_link": invite_link},
 			now=True,
 		)
-		self.db_set('email_sent_at', frappe.utils.now())
+		self.db_set("email_sent_at", frappe.utils.now())
 
 	def accept(self):
-		if self.status == 'Expired':
-			frappe.throw('Invalid or expired key')
+		if self.status == "Expired":
+			frappe.throw("Invalid or expired key")
 
 		user = self.create_user_if_not_exists()
 		user.append_roles(self.role)
 		user.save(ignore_permissions=True)
 		self.create_guest_access(user)
 
-		self.status = 'Accepted'
+		self.status = "Accepted"
 		self.accepted_at = frappe.utils.now()
 		self.save(ignore_permissions=True)
 
 	def create_guest_access(self, user):
-		if self.role == 'Gameplan Guest':
+		if self.role == "Gameplan Guest":
 			teams = frappe.parse_json(self.teams) if self.teams else []
 			for team in teams:
-				guest_access = frappe.get_doc(doctype='GP Guest Access')
+				guest_access = frappe.get_doc(doctype="GP Guest Access")
 				guest_access.user = user.name
 				guest_access.team = team
 				guest_access.save(ignore_permissions=True)
 
 			projects = frappe.parse_json(self.projects) if self.projects else []
 			for project in projects:
-				guest_access = frappe.get_doc(doctype='GP Guest Access')
+				guest_access = frappe.get_doc(doctype="GP Guest Access")
 				guest_access.user = user.name
 				guest_access.project = project
 				guest_access.save(ignore_permissions=True)
@@ -83,15 +82,16 @@ class GPInvitation(Document):
 			user = frappe.get_doc("User", self.email)
 		return user
 
+
 def expire_invitations():
-	''' expire invitations after 3 days '''
+	"""expire invitations after 3 days"""
 	from frappe.utils import add_days, now
 
 	days = 3
-	invitations_to_expire = frappe.db.get_all('GP Invitation',
-		filters={'status': 'Pending', 'creation': ['<', add_days(now(), -days)]}
+	invitations_to_expire = frappe.db.get_all(
+		"GP Invitation", filters={"status": "Pending", "creation": ["<", add_days(now(), -days)]}
 	)
 	for invitation in invitations_to_expire:
-		invitation = frappe.get_doc('GP Invitation', invitation.name)
-		invitation.status = 'Expired'
+		invitation = frappe.get_doc("GP Invitation", invitation.name)
+		invitation.status = "Expired"
 		invitation.save(ignore_permissions=True)
