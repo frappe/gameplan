4f167a94d3156ef11d6fd2cf3f4b3c4cee0d1588
Faris Ansari <netchamp.faris@gmail.com>
2023-07-25 21:16:10 +0530

    fix: frappeui vite plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
-import { getProxyOptions } from 'frappe-ui/src/utils/vite-dev-server'
-import { webserver_port } from '../../../sites/common_site_config.json'
+import frappeui from 'frappe-ui/vite'
 import Icons from 'unplugin-icons/vite'
 import Components from 'unplugin-vue-components/vite'
 import IconsResolver from 'unplugin-icons/resolver'
@@ -12,6 +11,7 @@ import LucideIcons from './lucideIcons'
 
 export default defineConfig({
   plugins: [
+    frappeui(),
     vue(),
     vueJsx(),
     Components({
@@ -26,10 +26,6 @@ export default defineConfig({
     }),
     visualizer({ emitFile: true }),
   ],
-  server: {
-    port: 8080,
-    proxy: getProxyOptions({ port: webserver_port }),
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
