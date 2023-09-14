469838cb3d9e495bf7e493715d6d0895956f6dd4
Faris Ansari <netchamp.faris@gmail.com>
2023-09-14 23:31:38 +0530

    fix: add gameplan logo in sidebar

--- frontend/src/components/UserDropdown.vue
+++ frontend/src/components/UserDropdown.vue
@@ -1,59 +1,74 @@
 <template>
-  <Dropdown
-    :options="[
-      {
-        icon: 'user',
-        label: 'My Profile',
-        route: {
-          name: 'PersonProfile',
-          params: { personId: $user().user_profile },
-        },
-      },
-      {
-        icon: 'settings',
-        label: 'Settings & Members',
-        onClick: () => showSettingsDialog(),
-        condition: () => $user().isNotGuest,
-      },
-      {
-        icon: 'log-out',
-        label: 'Log out',
-        onClick: () => logout(),
-      },
-    ]"
-  >
+  <Dropdown :options="dropdownItems">
     <template v-slot="{ open }">
       <button
-        class="flex w-full items-center space-x-2 rounded-md p-2 text-left"
-        :class="open ? 'bg-gray-300' : 'hover:bg-gray-200'"
+        class="flex w-[15rem] items-center rounded-md px-2 py-2 text-left"
+        :class="open ? 'bg-white shadow-sm' : 'hover:bg-gray-200'"
       >
-        <UserAvatar :user="$user().name" size="md" />
-        <span class="hidden text-base font-medium text-gray-900 sm:inline">
-          {{ $user().full_name }}
-        </span>
-        <LucideChevronDown class="hidden h-4 w-4 sm:inline" />
+        <GameplanLogo class="w-8 h-8 rounded" />
+        <div class="ml-2 flex flex-col">
+          <div class="text-base font-medium text-gray-900 leading-none">
+            Gameplan
+          </div>
+          <div class="mt-1 hidden text-sm text-gray-700 sm:inline leading-none">
+            {{ $user().full_name }}
+          </div>
+        </div>
+        <LucideChevronDown class="ml-auto hidden h-4 w-4 sm:inline" />
       </button>
     </template>
   </Dropdown>
 </template>
 <script>
-import { Dropdown, Link } from 'frappe-ui'
+import { h } from 'vue'
+import { Dropdown } from 'frappe-ui'
 import { showSettingsDialog } from '@/components/Settings/SettingsDialog.vue'
+import LucideCreditCard from '~icons/lucide/credit-card'
+import GameplanLogo from './GameplanLogo.vue'
 
 export default {
   name: 'UserDropdown',
   components: {
     Dropdown,
-    Link,
+    GameplanLogo,
   },
-  setup() {
-    return {
-      showSettingsDialog,
-    }
-  },
-  methods: {
-    logout() {
-      this.$session.logout.submit()
+  computed: {
+    dropdownItems() {
+      return [
+        {
+          icon: 'user',
+          label: 'My Profile',
+          route: {
+            name: 'PersonProfile',
+            params: { personId: this.$user().user_profile },
+          },
+        },
+        {
+          icon: 'settings',
+          label: 'Settings & Members',
+          onClick: () => showSettingsDialog(),
+          condition: () => this.$user().isNotGuest,
+        },
+        {
+          icon: () => h(LucideCreditCard),
+          label: 'Subscription',
+          condition: () =>
+            this.$user().isNotGuest &&
+            window.frappecloud_host &&
+            window.site_name,
+          onClick: () => {
+            window.open(
+              `${window.frappecloud_host}/dashboard/subscription/${window.site_name}`,
+              '_blank',
+            )
+          },
+        },
+        {
+          icon: 'log-out',
+          label: 'Log out',
+          onClick: () => this.$session.logout.submit(),
+        },
+      ]
     },
   },
 }
