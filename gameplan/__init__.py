036c43abb39f14641a62af62ac24410733a92ba5
Faris Ansari <netchamp.faris@gmail.com>
2023-09-14 23:32:59 +0530

    fix: better is_guest check

--- gameplan/__init__.py
+++ gameplan/__init__.py
@@ -5,7 +5,10 @@ __version__ = '0.0.1'
 def is_guest():
 	if frappe.session.user == 'Administrator':
 		return False
-	return 'Gameplan Guest' in frappe.get_roles()
+	roles = frappe.get_roles()
+	if 'Gameplan Member' in roles or 'Gameplan Admin' in roles:
+		return False
+	return 'Gameplan Guest' in roles
 
 def refetch_resource(cache_key: str | list, user=None):
 	frappe.publish_realtime(
