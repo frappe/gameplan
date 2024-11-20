4fc4d9cf23813c14ea1d3099a703d2de9586e64a
Faris Ansari <netchamp.faris@gmail.com>
2024-11-20 20:13:22 +0530

    fix(action-sheet): add close button and make scrollable

--- frontend/src/components/ActionSheet.vue
+++ frontend/src/components/ActionSheet.vue
@@ -4,8 +4,22 @@
     @click.self="emit('update:modelValue', false)"
     v-if="modelValue"
   >
-    <div v-if="modelValue" class="bg-white shadow-lg w-full rounded-t-xl slide-in-from-bottom">
-      <div class="py-4 text-lg font-bold border-b text-center">{{ title }}</div>
+    <div
+      v-if="modelValue"
+      class="bg-white max-h-screen overflow-auto shadow-lg w-full rounded-t-xl slide-in-from-bottom"
+    >
+      <div class="py-4 border-b text-center relative">
+        <div class="text-lg font-bold">
+          {{ title }}
+        </div>
+        <div class="absolute right-0 inset-y-0 flex items-center pr-4">
+          <Button variant="ghost" @click="emit('update:modelValue', false)">
+            <template #icon>
+              <LucideX class="text-gray-600" />
+            </template>
+          </Button>
+        </div>
+      </div>
       <div>
         <slot />
       </div>
