89145bbe35f2c3a8baf67f7ffb8be8e135a15588
Faris Ansari <netchamp.faris@gmail.com>
2024-12-10 01:10:14 +0530

    chore: update lucide

--- frontend/lucideIcons.js
+++ frontend/lucideIcons.js
@@ -2,10 +2,10 @@ import * as LucideIcons from 'lucide-static'
 
 let icons = {}
 for (const icon in LucideIcons) {
-  let iconSvg = LucideIcons[icon]
   if (icon == 'default') {
     continue
   }
+  let iconSvg = LucideIcons[icon]
 
   // set stroke-width to 1.5
   if (iconSvg && iconSvg.includes('stroke-width')) {
@@ -26,8 +26,14 @@ export default icons
 function camelToDash(key) {
   // barChart2 -> bar-chart-2
   let withNumber = key.replace(/[A-Z0-9]/g, (m) => '-' + m.toLowerCase())
+  if (withNumber.startsWith('-')) {
+    withNumber = withNumber.substring(1)
+  }
   // barChart2 -> bar-chart2
   let withoutNumber = key.replace(/[A-Z]/g, (m) => '-' + m.toLowerCase())
+  if (withoutNumber.startsWith('-')) {
+    withoutNumber = withoutNumber.substring(1)
+  }
 
   if (withNumber !== withoutNumber) {
     // both are required because unplugin icon resolver doesn't put a dash before numbers
