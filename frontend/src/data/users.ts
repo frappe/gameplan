61ff6485a9995a1df913edec27da6dc0d057bfa9
Faris Ansari <netchamp.faris@gmail.com>
2024-12-30 13:59:07 +0530

    refactor: users to useCall

--- frontend/src/data/users.ts
+++ frontend/src/data/users.ts
@@ -1,12 +1,29 @@
-import { createResource } from 'frappe-ui'
 import { computed, reactive } from 'vue'
+import { useCall } from 'frappe-ui/src/data-fetching'
 import router from '@/router'
 import { session } from './session'
 
-let usersByName = reactive({})
-export let users = createResource({
-  url: 'gameplan.api.get_user_info',
-  cache: 'Users',
+let usersByName = reactive<Record<string, UserInfo>>({})
+
+interface UserInfo {
+  name: string
+  email: string
+  enabled: number
+  user_image: string
+  full_name: string
+  user_type: string
+  user_profile: string
+  image_background_color: string
+  is_image_background_removed: number
+  role: 'Gameplan Admin' | 'Gameplan Member' | 'Gameplan Guest'
+  isGuest?: boolean
+  isNotGuest?: boolean
+  isDisabled?: boolean
+}
+
+export let users = useCall<UserInfo[]>({
+  url: '/api/v2/method/gameplan.api.get_user_info',
+  cacheKey: 'Users',
   initialData: [],
   transform(users) {
     for (let user of users) {
@@ -18,32 +35,43 @@ export let users = createResource({
     return users
   },
   onError(error) {
-    if (error && error.exc_type === 'AuthenticationError') {
+    if (error && error.type === 'AuthenticationError') {
       router.push('/login')
     }
   },
+  immediate: false,
 })
 
-export function getUser(email) {
+export function useUser(email?: string | null) {
   if (!email || email === 'sessionUser') {
     email = session.user
   }
+  if (!email) return getPlaceholderUser('guest@example.com', 'Guest')
   if (!usersByName[email]) {
-    usersByName[email] = {
-      name: email,
-      email: email,
-      full_name: email.split('@')[0],
-      user_image: null,
-      role: null,
-    }
+    usersByName[email] = getPlaceholderUser(email, email.split('@')[0])
   }
   return usersByName[email]
 }
 
+function getPlaceholderUser(email: string, full_name: string) {
+  return {
+    name: email,
+    email: email,
+    full_name: full_name,
+    user_image: '',
+    role: 'Gameplan Member',
+    enabled: 1,
+    user_profile: '',
+    image_background_color: '',
+    is_image_background_removed: 0,
+    user_type: 'Website User',
+  } as UserInfo
+}
+
 export let activeUsers = computed(() => {
-  return users.data.filter((user) => user.enabled)
+  return (users.data || []).filter((user) => user.enabled)
 })
 
 export function useSessionUser() {
-  return getUser('sessionUser')
+  return useUser('sessionUser')
 }
