4f6c6ae16215340e67892f98b01013a7159b525a
Faris Ansari <netchamp.faris@gmail.com>
2025-03-13 08:56:57 +0530

    fix: set search2 route on mobile search

--- frontend/src/components/MobileLayout.vue
+++ frontend/src/components/MobileLayout.vue
@@ -72,7 +72,7 @@ export default {
         {
           name: 'Search',
           icon: LucideSearch,
-          route: { name: 'Search' },
+          route: { name: 'Search2' },
           isActive: this.$route.name === 'Search',
           condition: () => this.$user().isNotGuest,
         },
