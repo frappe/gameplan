4edf8195c1a0a6c2b79c2b514933bc6b75db6439
Faris Ansari <netchamp.faris@gmail.com>
2025-07-02 17:15:51 +0530

    chore: mariadb installation

--- .github/helper/install_dependencies.sh
+++ .github/helper/install_dependencies.sh
@@ -8,7 +8,7 @@ curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyr
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 
 sudo apt update
-sudo apt install libcups2-dev redis mariadb-client-10.6
+sudo apt install libcups2-dev redis mariadb-client
 
 install_wkhtmltopdf() {
   wget -q https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
